a4f72af2c0a1ccdf8e8ca84ca3a3137c5134b289
Nicholas Anderson <nla2121@tc.columbia.edu>
2023-01-20 09:31:02 -0500

    Create class_roster.py
    Adding a file which we can all modify as we learn how to use git!

---
+++ Programs/class_roster.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+
+This code creates a class roster ! 
+
+"""
+
+class_roster = []
+
+# Exercise: For each student, add one line of code below where you "append" your name
+# to the class_roster list! Make sure your code is added above the print statement!
+
+class_roster.append("Nick Anderson")
+
+
+
+
+
+
+
+
+print(class_roster)
